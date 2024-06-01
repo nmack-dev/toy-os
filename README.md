34ae9adf97471bb66b1959d858815d6cabca5219
Nathan Mack <nathan@macktribe.com>
2024-06-01 16:03:12 -0500

    Add initial vga buffer implementation and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# toy-os
+My personal repo for writing a kernel in Rust, following along Phillip Opperman's tutorial.
+
+[Writing an OS in Rust]("https://os.phil-opp.com/")
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,25 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 // disable name mangling bc otherwise the rust compiler
 // will give this function a random name, and we need to
 // tell the linker the kernel entry point.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // cast to a raw ptr
+    let vga_buffer = 0xb8000 as *mut u8;
+    
+    // itr over chars, w/ enumeration
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // write char & cyan color to vga buffer address
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
