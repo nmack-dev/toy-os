ebcaae8cbaaf5468cef87f5e00b71af4a77287b0
Nathan Mack <nathan@macktribe.com>
2024-06-02 19:18:16 -0500

    Add vga_buffer module for printing text

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,27 @@
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
+mod vga_buffer;
+
+// static HELLO: &[u8] = b"Hello World!";
 
 // disable name mangling bc otherwise the rust compiler
 // will give this function a random name, and we need to
 // tell the linker the kernel entry point.
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // cast to a raw ptr
-    let vga_buffer = 0xb8000 as *mut u8;
+    // // cast to a raw ptr
+    // let vga_buffer = 0xb8000 as *mut u8;
     
-    // itr over chars, w/ enumeration
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            // write char & cyan color to vga buffer address
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // // itr over chars, w/ enumeration
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         // write char & cyan color to vga buffer address
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    vga_buffer::test_func();
 
     loop {}
 }
